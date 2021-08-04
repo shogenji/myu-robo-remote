a476a81ee488fe893b9ed93475b12a687419408f
shogenji <ruirui@ruirui.jp>
2021-08-04 15:40:44 +0900

    handleDisconnectedDevice

--- myu-robo-remote.js
+++ myu-robo-remote.js
@@ -24,7 +24,6 @@ async function connect() {
         }
         // Wait for the HID connection to open.
 	    await device.open();
-        timer = setInterval(checkDevice, 3000);
         document.getElementById("deviceStatus").innerText = device.productName + "に接続しました。";
     } catch (error) {
         console.error(error.name, error.message);
@@ -81,25 +80,11 @@ async function remoteMouseup() {
     // console.log(data);
 }
 
-async function checkDevice() {
-    if (!device) return;
-
-	const reportId = 0x00;
-    // const data = Uint8Array.from([  7,  94,   0, 222, 119,  74,  10, 226, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0]);
-    const data = Uint8Array.from([  2,   5,   0, 222, 119,  74,  10, 226, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0]);
-    
-    try {
-        await device.sendReport(reportId, new Uint8Array(data));
-        console.log("connected");
-    } catch (err) {
-        device = undefined;
-        clearInterval(timer);
-        document.getElementById("deviceStatus").innerText = "接続されていません。";
-        console.log(err);
-    }      
-}
-
 function startup() {
+    if (!("hid" in navigator)) {
+        document.getElementById("deviceStatus").innerText = "WebHIDに未対応です。";
+    }
+
     const btnConnect = document.getElementById('btnConnect');
     btnConnect.addEventListener('mouseup', connect, false);
     btnConnect.addEventListener('touchend', connect, false);
@@ -131,6 +116,26 @@ function startup() {
 }
 
 document.addEventListener("DOMContentLoaded", startup);
+
+function handleConnectedDevice(e) {
+    console.log("Device connected: " + e.device.productName);
+}
+
+function handleDisconnectedDevice(e) {
+    console.log("Device disconnected: " + e.device.productName);
+
+    device = undefined;
+    document.getElementById("deviceStatus").innerText = "接続されていません。";
+}
+
+function handleInputReport(e) {
+    console.log(e.device.productName + ": got input report " + e.reportId);
+    console.log(new Uint8Array(e.data.buffer));
+}
+  
+navigator.hid.addEventListener("connect", handleConnectedDevice);
+navigator.hid.addEventListener("disconnect", handleDisconnectedDevice);
+
 
 window.oncontextmenu = function(event) {
     event.preventDefault();
