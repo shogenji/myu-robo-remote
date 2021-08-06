ac990ac7ad37d856821183537507637ba302a798
shogenji <ruirui@ruirui.jp>
2021-08-06 15:32:50 +0900

    Update myu-robo-remote.js
    mouseup

--- myu-robo-remote.js
+++ myu-robo-remote.js
@@ -103,7 +103,7 @@ function startup() {
 
     const btnConnect = document.getElementById('btnConnect');
     btnConnect.addEventListener('mouseup', connect, false);
-    btnConnect.addEventListener('touchend', connect, false);
+    // btnConnect.addEventListener('touchend', connect, false);
 
     const btnForward = document.getElementById('btnForward');
     const btnBackward = document.getElementById('btnBackward');
@@ -111,24 +111,24 @@ function startup() {
     const btnTurnRight = document.getElementById('btnTurnRight');
     
     btnForward.addEventListener('mousedown', remoteForward, false);
-    btnForward.addEventListener('touchstart', remoteForward, false);
+    // btnForward.addEventListener('touchstart', remoteForward, false);
     btnForward.addEventListener('mouseup', remoteMouseup, false);
-    btnForward.addEventListener('touchend', remoteMouseup, false);
+    // btnForward.addEventListener('touchend', remoteMouseup, false);
 
     btnBackward.addEventListener('mousedown', remoteBackward, false);
-    btnBackward.addEventListener('touchstart', remoteBackward, false);
+    // btnBackward.addEventListener('touchstart', remoteBackward, false);
     btnBackward.addEventListener('mouseup', remoteMouseup, false);
-    btnBackward.addEventListener('touchend', remoteMouseup, false);
+    // btnBackward.addEventListener('touchend', remoteMouseup, false);
 
     btnTurnLeft.addEventListener('mousedown', remoteTurnLeft, false);
-    btnTurnLeft.addEventListener('touchstart', remoteTurnLeft, false);
+    // btnTurnLeft.addEventListener('touchstart', remoteTurnLeft, false);
     btnTurnLeft.addEventListener('mouseup', remoteMouseup, false);
-    btnTurnLeft.addEventListener('touchend', remoteMouseup, false);
+    // btnTurnLeft.addEventListener('touchend', remoteMouseup, false);
     
     btnTurnRight.addEventListener('mousedown', remoteTurnRight, false);
-    btnTurnRight.addEventListener('touchstart', remoteTurnRight, false);
+    // btnTurnRight.addEventListener('touchstart', remoteTurnRight, false);
     btnTurnRight.addEventListener('mouseup', remoteMouseup, false);
-    btnTurnRight.addEventListener('touchend', remoteMouseup, false);
+    // btnTurnRight.addEventListener('touchend', remoteMouseup, false);
 }
 
 document.addEventListener("DOMContentLoaded", startup);
